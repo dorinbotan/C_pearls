d5ac0d19c2f53e119606d2e7b99dee8ac5dffd2a
Dorin Botan <dorinbotan@mail.ru>
2018-09-25 15:20:21 +0200

    Typo

--- Uninitialized_assignment/main.c
+++ Uninitialized_assignment/main.c
@@ -5,7 +5,7 @@ int main()
     long i = i; // No error nor warning here when compiled with GCC & Clang
 
 /*
-    Code above is simmilar to:
+    Code above is similar to:
 
     long i;
     i = i;
