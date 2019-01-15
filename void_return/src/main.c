50317519a683ab072df2a56ab1333391b67ff761
Dorin Botan <dorinbotan@mail.ru>
2019-01-15 08:55:02 +0100

    Remove tabs to fix formatting

--- void_return/src/main.c
+++ void_return/src/main.c
@@ -7,8 +7,8 @@ void foo()
 	printf("Hello world!\n");
 }
 
-/* Will print out 13, which is the return value of printf       			 */
-/* Undefined behavior                                           			 */
+/* Will print out 13, which is the return value of printf        	     */
+/* Undefined behavior                                                        */
 /* Reason for this happening, on a x86 system is that printf would place the */
 /* return value in eax, from where later 'a' would read it's value, assuming */
 /* it's the return value of foo()                                            */
